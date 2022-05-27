880b9cc3f01c03f39790515d6a779d8fe51d1fc2
Axelcg9 <axelcg9@gmail.com>
2022-05-27 13:24:20 -0500

    Se modificó el README

--- Js/main.js
+++ Js/main.js
@@ -1,9 +1,10 @@
 // Soliicitar 3 números (entre el 1 y el 100) y definir cual es el mayor.
-// var n1,n2,n3,t, mayor;
+// let n1,n2,n3,t, mayor;
 
 // n1 = parseFloat(prompt("Número 1 menor que 100"));
 // n2 = parseFloat(prompt("Número 2 menor que 100"));
 // n3 = parseFloat(prompt("Número 3 menor que 100"));
+
 
 // if (n1 > n2){
 //     t = n1;
@@ -20,19 +21,42 @@
 // document.write("El mayor es " + mayor);
 
 //
+// Solicitar 3 números ( entre el 1 y el 100) y definir cual es el mayor.
+
+// let n1,n2,n3,t, menor;
+
+// n1 = parseFloat(prompt("Número 1 menor que 100"));
+// n2 = parseFloat(prompt("Número 2 menor que 100"));
+// n3 = parseFloat(prompt("Número 3 menor que 100"));
+
+// if (n1 > n2){
+//     t = n1;
+// }else{
+//     t = n2;
+// }
+
+// if (t < n3){
+//     mayor = t;
+// }else{
+//     mayor = n3;
+// }
+
+// document.write("El menor es " + menor);
 
 // 4. Solicitar un entero (entre el 100 y el 200) y determinar si es multiplo de 3.
 // 5.Elaborar un algoritmo para leer 3 números y determinar si uno es la suma de los otros 2.
 // 6. Elabora un algoritmo para leer un número y determinar si es par o impar.
 
 
-// var x = Math.random()*(100, 200)
+// var x = Math.random()*(101) + 100 
 // console.log (parseInt (x))
 //      if (x<=200 &&x>=100) {
 //          console.log ("Tú número está dentro del rango")
 //          let res=x%3
+//         //  if
 //          console.log ("Tú número es multiplo de 3", res)
 //         }
+// Fuera del rango
 
 // let n1 = parseInt(prompt("Número 1"));
 // console.log ("El valor de n1 es ", n1)
@@ -46,14 +70,15 @@
 // }else{
 //     console.log("La suma de ",n1,"más",n2, "no es igual a ",n3);
 // }
+// res2, res3
 
 
-let a1 = parseInt(prompt("Ingresa un número"))
-console.log ("El valor ingresado de a1", a1)
-if(a1%2==0){
-    console.log("El número ",a1,"es par")
-}else{
-    console.log("El número",a1, "es impar")
-}
+// let a1 = parseInt(prompt("Ingresa un número"))
+// console.log ("El valor ingresado de a1", a1)
+// if(a1%2==0){
+//     console.log("El número ",a1,"es par")
+// }else{
+//     console.log("El número",a1, "es impar")
+// }
 
       //3,4,5
---
+++ README.md
@@ -0,0 +1,19 @@
+# Ejercicios de Algoritmos implementados en Javascript
+
+ImplementaciÃn de los ejercicios de algoritmos con Javascript
+
+## Se integran los siguientes algoritmos resueltos:
+
+1. Solicitar 3 nÃmeros (entre el 1 y el 100) y definir cual es el mayor
+
+2.Solicitar 3 nÃºmeros (entre el 1 y el 100) y definir el menor de tres nÃmeros
+
+_3.Adivinar un nÃmero entre el 1 y el 100 en el menor nÃmero de pasos posibles_
+
+4.Solicitar un entero (entre el 100 y el 200) y determinar si es multiplo de 3
+5.Elaborar un algoritmo para leer 3 nÃmeros y determinar si uno es la suma de los otros dos
+6.Elabora un algoritmo para leer un nÃmero y determinar si es par o impar
+
+---
+
+>Nota: El algorimo #3 se encuentra en desarrollo.
